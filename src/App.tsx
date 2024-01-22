d336e7f04e59cfb1031278246dd01133534a80fe
Ben Houston <neuralsoft@gmail.com>
2024-01-22 14:27:32 -0500

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useErrorBoundary } from 'use-error-boundary'
 import { Route, useRoute, Redirect } from 'wouter'
 
 import { Global, Loading, Page, DemoPanel, Dot, Error } from './styles'
+import { ContinuousEventPriority } from 'react-reconciler/constants';
 
 import * as demos from './demos'
 
